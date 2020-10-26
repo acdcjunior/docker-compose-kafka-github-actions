656cd5163a8bf051a83447bebb98d94c6a5dacc2
Antonio Jr <acdcjunior@gmail.com>
2020-10-26 11:36:33 -0300

    pipeline: fix oracle script

--- .github/workflows/pipeline-with-kafka/oracle-xe-11g/oracle-start.sh
+++ .github/workflows/pipeline-with-kafka/oracle-xe-11g/oracle-start.sh
@@ -5,5 +5,5 @@ docker run \
  -p 1521:1521 \
  --name oracle-xe-11g \
  -e ORACLE_ALLOW_REMOTE=true \
- -v .github/workflows/pipeline-with-kafka/oracle-xe-11g/seed:/docker-entrypoint-initdb.d \
+ -v ".github/workflows/pipeline-with-kafka/oracle-xe-11g/seed":"/docker-entrypoint-initdb.d" \
  wnameless/oracle-xe-11g-r2
